8d6546fcb2a32bf5cb248389fe6492e29558997a
Anthony Baker <metatype@users.noreply.github.com>
2023-11-17 10:12:50 -0800

    Add support for GemFire Vector Store

--- src/main/java/org/cloudfoundry/samples/music/config/ai/GemFireAiConfiguration.java
+++ src/main/java/org/cloudfoundry/samples/music/config/ai/GemFireAiConfiguration.java
@@ -16,29 +16,24 @@
 
 package org.cloudfoundry.samples.music.config.ai;
 
-import org.springframework.ai.client.AiClient;
 import org.springframework.ai.embedding.EmbeddingClient;
-import org.springframework.ai.retriever.VectorStoreRetriever;
-import org.springframework.ai.vectorstore.PgVectorStore;
+import org.springframework.ai.vectorstore.GemFireVectorStore;
+import org.springframework.ai.vectorstore.GemFireVectorStore.GemFireVectorStoreConfig;
 import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.ai.vectorstore.InMemoryVectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
  *
- * @author Long Nguyen
+ * @author Anthony Baker
  */
 @Configuration
-@Profile("!(postgres || gemfire)")
-public class InMemoryAiConfiguration {
+@Profile("gemfire")
+public class GemFireAiConfiguration {
 
 	@Bean
 	public VectorStore vectorStore(EmbeddingClient embeddingClient) {
-		return new InMemoryVectorStore(embeddingClient);
+		return new GemFireVectorStore(GemFireVectorStoreConfig.builder().withHost("127.0.0.1").withIndex("spring-music-ai").build(), embeddingClient);
 	}
-
-
 }
